0d188a3830960bd6fa721d2febca59fd15655fd8
XiNGRZ <chenxingyu92@gmail.com>
2018-06-03 12:45:00 +0800

    osborn: Apply configs changes from stock
    Change-Id: I252c0bcc290d93a1f93f0067730685baf6bc1441

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,7 +21,8 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "QCOM-BTD"
+#define BTM_DEF_LOCAL_NAME   "Smartisan U3 Pro"
+#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    20
