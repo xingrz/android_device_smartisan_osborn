1af9c1239a85ab894a80bd8b63beed4b7e16e259
XiNGRZ <chenxingyu92@gmail.com>
2018-06-18 21:36:06 +0800

    osborn: Build LiveDisplay
    Change-Id: Idd73084d8c4f2e16ed2a34035decfc75870d1a3c

--- device.mk
+++ device.mk
@@ -296,6 +296,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.osborn
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.mokee.livedisplay@1.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
