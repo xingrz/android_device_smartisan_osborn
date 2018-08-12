2e40da1eec93ad527e1de88d0560b6f055008280
LuK1337 <priv.luk@gmail.com>
2018-08-12 20:51:09 +0200

    osborn: Fix build of librecovery_updater_osborn
    Change-Id: I308fca4987b049266ebaae04de8b3e0efbf836ba

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -5,7 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := librecovery_updater_osborn
 LOCAL_MODULE_TAGS := eng
 
-LOCAL_C_INCLUDES := $(call project-path-for,recovery)
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 
