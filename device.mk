acd4866019dd9242932569f835a1f0bd394047a7
XiNGRZ <chenxingyu92@gmail.com>
2018-10-06 17:16:50 +0800

    osborn: Upgrade NXP NFC 1.1
    Change-Id: Ic7fea11320f14aae2c442ff330fc49ca76a2e344

--- device.mk
+++ device.mk
@@ -319,14 +319,14 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-impl \
     vendor.nxp.hardware.nfc@1.0 \
     vendor.nxp.hardware.nfc@1.0-impl \
-    vendor.nxp.hardware.nfc@1.0-service
+    vendor.nxp.hardware.nfc@1.1-service
 
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     com.gsma.services.nfc \
     com.nxp.nfc.nq \
     libnqnfc-nci \
-    nfc_nci.nqx.default \
+    nfc_nci.nqx.default.hw \
     libnqp61-jcop-kit \
     NQNfcNci \
     nqnfcee_access.xml \
