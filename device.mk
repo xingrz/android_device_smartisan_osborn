f7f5c254bdd76187931b11a0657df8ef455e49de
Martincz Gao <martincz@mokeedev.com>
2018-10-17 15:22:26 +0800

    osborn: Derp
    Change-Id: I05b22c9c2c866c9b26db76db9c568860d2e0a302

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-mk
+    $(LOCAL_PATH)/overlay-mokee
 
 # Permissions
 PRODUCT_COPY_FILES += \
