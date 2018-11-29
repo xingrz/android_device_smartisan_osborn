45596c15af82ae79a8ded6b4e6211590e43a6c0f
XiNGRZ <chenxingyu92@gmail.com>
2018-11-29 19:20:52 +0800

    osborn: Update blobs from 6.1.2.1-201810292004-user-ob-508d416672
    Change-Id: I48bbf288514b2daec02b0ebc370b8f91d616795b

--- mokee.mk
+++ mokee.mk
@@ -39,7 +39,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="osborn" \
     PRIVATE_BUILD_DESC="osborn-user 7.1.1 NGI77B 1 dev-keys"
 
-BUILD_FINGERPRINT := Android/osborn/osborn:7.1.1/NGI77B/1536068833:user/dev-keys
+BUILD_FINGERPRINT := Android/osborn/osborn:7.1.1/NGI77B/1540814905:user/dev-keys
 
 # Sign bootable images
 PRODUCT_SUPPORTS_BOOT_SIGNER := true
