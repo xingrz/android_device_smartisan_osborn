c85a38c872a4c8a7cff633e2b2ba597810b33370
XiNGRZ <chenxingyu92@gmail.com>
2018-12-31 16:22:52 +0800

    osborn: Import device-specific Goodix fingerprint blobs
    Change-Id: Ie6188c0cc72881c1a0afcaefd99b6e36be4a67aa

--- extract-files.sh
+++ extract-files.sh
@@ -21,4 +21,14 @@ export DEVICE=osborn
 export DEVICE_COMMON=sdm660-common
 export VENDOR=smartisan
 
+export DEVICE_BRINGUP_YEAR=2018
+
 ./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+MK_ROOT="$MY_DIR"/../../..
+
+BLOB_ROOT="$MK_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+sed -i 's|\x00goodixfingerprint\x00|\x00fingerprint\x00\x00\x00\x00\x00\x00\x00|' $BLOB_ROOT/vendor/lib64/hw/fingerprint.goodix.so
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -21,4 +21,6 @@ export DEVICE=osborn
 export DEVICE_COMMON=sdm660-common
 export VENDOR=smartisan
 
+export DEVICE_BRINGUP_YEAR=2018
+
 ./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
